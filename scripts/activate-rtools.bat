2b670eef5c07e1e9a53bc9ccb69d8a6795ab65f7
mattjmeier <matthew.james.meier@gmail.com>
2026-08-16 09:35:09 -0400

    feat: tell pixi to use local rtools install

---
+++ scripts/activate-rtools.bat
@@ -0,0 +1,4 @@
+@echo off
+set "PATH=C:\rtools45\usr\bin;C:\rtools45\x86_64-w64-mingw32.static.posix\bin;%PATH%"
+set "R_TOOLS_SOFT=C:\rtools45\x86_64-w64-mingw32.static.posix"
+set "LOCAL_SOFT=C:\rtools45\x86_64-w64-mingw32.static.posix"
